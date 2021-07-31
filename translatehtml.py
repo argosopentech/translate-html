431ebdc3aef3b1f0a5c2f75b69e44a57d0d6bd31
P.J. Finlay <pjappdevelopment@gmail.com>
2021-07-31 15:13:28 -0500

    HTML translation working

--- translatehtml.py
+++ translatehtml.py
@@ -8,49 +8,71 @@ from argostranslate import package, translate
 installed_languages = translate.get_installed_languages()
 translation_en_es = installed_languages[0].get_translation(installed_languages[1])
 
-t = Tag(['I went to ', TagLiteral('Paris'), ' last summer.'])
+read_file = True
 
-translated_tags = translate_tags(translation_en_es, t).children()
-for translated_tag in translated_tags:
-    print(str(translated_tag))
+if not read_file:
+    html_doc = """
+    <html><head><title>The Dormouse's story</title></head>
+    <body>
+    <p class="title"><b>The Dormouse's story</b></p>
 
+    <p class="story">Once upon a time there were three little sisters; and their names were
+    <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+    <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+    <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+    and they lived at the bottom of a well.</p>
 
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
+    <p class="story">...</p>
+    """
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+    soup = BeautifulSoup(html_doc, "html.parser")
+else:
+    soup = BeautifulSoup(open("index.html"))
 
-<p class="story">...</p>
-"""
-
-soup = BeautifulSoup(html_doc, 'html.parser')
 
 class HTMLTag(Tag):
     pass
-            
+
+
 class HTMLTagLiteral(TagLiteral):
     pass
+
 
 def itag_of_soup(soup):
     if isinstance(soup, bs4.element.NavigableString):
         return str(soup)
     elif len(soup.contents) == 0:
         to_return = HTMLTag(list())
-    elif len(soup.contents) == 1 and isinstance(soup.contents[0], bs4.element.NavigableString):
+    elif len(soup.contents) == 1 and isinstance(
+        soup.contents[0], bs4.element.NavigableString
+    ):
         to_return = HTMLTagLiteral(str(soup.contents[0]))
     else:
         to_return = HTMLTag([itag_of_soup(content) for content in soup.contents])
     to_return.soup = soup
     return to_return
 
+
 htmltag = itag_of_soup(soup)
+
+print(htmltag)
 
 translated_tags = translate_tags(translation_en_es, htmltag)
 
-print(translated_tags.text())
+print(translated_tags)
+
+
+def soup_of_itag(itag):
+    if type(itag) == str:
+        return bs4.element.NavigableString(itag)
+    soup = itag.soup
+    if isinstance(itag, argostranslate.tags.TagLiteral):
+        soup.contents = [bs4.element.NavigableString(itag.text())]
+    elif isinstance(itag, argostranslate.tags.Tag):
+        soup.contents = [soup_of_itag(content) for content in itag.children()]
+    return soup
+
+
+translated_soup = soup_of_itag(translated_tags)
+
+print(translated_soup)
