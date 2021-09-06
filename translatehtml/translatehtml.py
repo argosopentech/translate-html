4967dbeea597372b8911821703eee7ccd7109540
Dingedi <64697405+dingedi@users.noreply.github.com>
2021-09-06 21:38:00 +0200

    Handle translate attribute
    Allows not to translate the elements including the attribute translate='no'

--- translatehtml/translatehtml.py
+++ translatehtml/translatehtml.py
@@ -30,7 +30,7 @@ def itag_of_soup(soup):
     """
     if isinstance(soup, bs4.element.NavigableString):
         return str(soup)
-    translateable = soup.name not in NON_TRANSLATEABLE_TAGS
+    translateable = soup.name not in NON_TRANSLATEABLE_TAGS and soup.get('translate') != "no"
     to_return = Tag([itag_of_soup(content) for content in soup.contents], translateable)
     to_return.soup = soup
     return to_return
